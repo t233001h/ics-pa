941993e7f6709e0c7b69213607e7fc2c1774de07
tracer-ics2025 <tracer@njuics.org>
2026-03-23 11:18:13 +0800

    >  compile NEMU 2312200000 张三 Linux house-VMware-Virtual-Platform 6.17.0-19-generic #19~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 23:08:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:18:13 up  1:03,  1 user,  load average: 1.91, 1.71, 1.48

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -64,7 +64,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  {"si [N]", "Step N instructions", cmd_si},
+  {"si", "Step N instructions", cmd_si},
 
   /* TODO: Add more commands */
 
